038ffe4c0da3b86a60892dff63ecd21f153b1de4
Daniel Slowinski <dslowin@gmail.com>
2013-12-04 17:01:30 -0600

    more template type stuff

--- public/js/infoBubbleC.js
+++ public/js/infoBubbleC.js
@@ -26,30 +26,63 @@ var InfoBubbleC = Backbone.Model.extend({
   	}
   },
   setupJob: function(job) {
+    this.clearLoaded();
   	this.set('job',job);
   	var removeCount = this.infoBubble.tabs_.length;
   	for (var i = removeCount - 1; i >= 0; i--) {
   		this.infoBubble.removeTab(0);
   	};
-  	var customer = job.get('customerUser');
-  	var customerData = 'No customer assigned';
-  	if (customer) {
-  		customerData = JSON.stringify(customer.toJSON());
-  	};
-  	var serviceCenter = job.get('serviceCenter');
-  	var serviceCenterData = 'No service center assigned';
-  	if (serviceCenter) {
-  		serviceCenterData = JSON.stringify(serviceCenter.toJSON());
-  	}
-  	var driver = job.get('driver');
-  	var driverData = 'No service center assigned';
-  	if (driver) {
-  		driverData = JSON.stringify(driver.toJSON());
-  	}
-  	this.infoBubble.addTab('Customer',customerData);
-  	this.infoBubble.addTab('Service Center',serviceCenterData);
-  	this.infoBubble.addTab('Driver',driverData);
-  	this.infoBubble.open(map,this.get('marker'));
+  	this.addCustomerTab(job.get('customerUser'));
+  	this.addServiceCenterTab(job.get('serviceCenter'));
+  	this.addDriverTab(job.get('driver'));
+  },
+  addCustomerTab: function(customer) {
+    this.finalLoadCount++;
+    var customerData = 'No customer assigned';
+    if (customer) {
+      customerData = JSON.stringify(customer.toJSON());
+    }
+    var self = this;
+    var data = {}
+    loadManager.loadHTML('customerInfoBubble.html',data, function(html) {
+      //self.$el.html(html);
+      self.infoBubble.addTab('Customer',html);
+      self.finishRender();
+    });
+  },
+  addServiceCenterTab: function(serviceCenter) {
+    this.finalLoadCount++;
+    var serviceCenterData = 'No service center assigned';
+    if (serviceCenter) {
+      serviceCenterData = JSON.stringify(serviceCenter.toJSON());
+    }
+    var self = this;
+    var data = {}
+    loadManager.loadHTML('serviceCenterInfoBubble.html',data, function(html) {
+      //self.$el.html(html);
+      self.infoBubble.addTab('Service Center',html);
+      self.finishRender();
+    });
+  },
+  addDriverTab: function(driver) {
+    this.finalLoadCount++;
+    var driverData = 'No service center assigned';
+    if (driver) {
+      driverData = JSON.stringify(driver.toJSON());
+    }
+    var self = this;
+    var data = {}
+    loadManager.loadHTML('driverInfoBubble.html',data, function(html) {
+      //self.$el.html(html);
+      self.infoBubble.addTab('Driver',html);
+      self.finishRender();
+    });
+  },
+  finishRender: function() {
+    this.loadCount++;
+    console.log(this.finalLoadCount,this.loadCount)
+    if (this.finalLoadCount === this.loadCount) {
+      this.infoBubble.open(map,this.get('marker'));
+    }
   }
-
 });
