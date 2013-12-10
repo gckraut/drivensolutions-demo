881ac53a7dd42385962032d5c49c5760209080d8
Daniel Slowinski <dan@slowinskisolutions.com>
2013-12-09 22:23:02 -0600

    twilio

--- web.js
+++ web.js
@@ -2,31 +2,14 @@ var path = require('path');
 var express = require("express");
 var app = express();
 var twilio = require('twilio');
+var ejs = require('ejs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-app.get('/cc', function(req, res) {
- 
-    // Create an object which will generate a capability token
-    // Replace these two arguments with your own account SID
-    // and auth token:
-    var capability = new twilio.Capability(
-        'AC4f89351042185b1bf4b0e37bc853be88',
-        '8df2133ca1415a4595be15a653188daf'
-    );
- 
-    // Give the capability generator permission to accept incoming
-    // calls to the ID "kevin"
-    capability.allowClientIncoming('cc');
- 
-    // Render an HTML page which contains our capability token
-    res.render('index.ejs', {
-        token:capability.generate()
-    });
-});
+
 
 // var capability = new twilio.Capability(
 //         process.env.TWILIO_ACCOUNT_SID,
@@ -53,4 +36,24 @@ app.configure(function(){
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+app.get('/cc', function(req, res) {
+ 
+    // Create an object which will generate a capability token
+    // Replace these two arguments with your own account SID
+    // and auth token:
+    var capability = new twilio.Capability(
+        'AC4f89351042185b1bf4b0e37bc853be88',
+        '8df2133ca1415a4595be15a653188daf'
+    );
+ 
+    // Give the capability generator permission to accept incoming
+    // calls to the ID "kevin"
+    capability.allowClientIncoming('cc');
+ 
+    // Render an HTML page which contains our capability token
+    res.render('index.ejs', {
+        token:capability.generate()
+    });
 });
